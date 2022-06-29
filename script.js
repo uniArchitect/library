308c2b2bbd1375d5054439414023d5ae47baba54
uniArchitect <hancho93@gmail.com>
2022-06-28 22:33:26 -0400

    myLibrary cannot be redeclared. In console, all object properties can be set by declaring a new constructor

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function book(name, author, pages, hasRead) {
     this.pages = pages;
     this.hasRead = hasRead;
 }
+
+// myLibrary will not be redeclared. Figure out why. 
+
+book.prototype.dataEntry = function() {
+    let myLibrary = [];
+    myLibrary.push(this.name, this.author, this.pages, this.hasRead);
+    console.log(myLibrary);
+}

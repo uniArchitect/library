6271dde102864e13d18b3ccf21d5124aeab1f62e
uniArchitect <hancho93@gmail.com>
2022-07-07 22:39:42 -0400

    Link up global scope variables with constructor values

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+// GLOBAL SCOPE
+
+// '#' is the id selector
+const NAME_INPUT = document.querySelector('#name') 
+const AUTHOR_INPUT = document.querySelector('#author')
+const PAGE_INPUT = document.querySelector('#page-count')
+const READ_INPUT = document.querySelector('#read-status')
+const SUBMIT_BUTTON = document.querySelector('#book-submit')
 let myLibrary = [];
 
-function book(name, author, pages, hasRead) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
+function book(NAME_INPUT, AUTHOR_INPUT, PAGE_INPUT, READ_INPUT) {
+    this.name = NAME_INPUT;
+    this.author = AUTHOR_INPUT;
+    this.pages = PAGE_INPUT;
+    this.hasRead = READ_INPUT;
 }
 
 // Defined a variable for a created new book
@@ -18,4 +26,6 @@ book.prototype.addBookToLibrary = function(addedBook) {
     console.log(myLibrary);
 }
 
-const SUBMIT_BUTTON = document.querySelector('[book-submit]')
+// SUBMIT_BUTTON.addEventListener('click', () => {
+
+// })

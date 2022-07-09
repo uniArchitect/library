8cdd7d6e8168af3ce0c3f9ed2eba84f64052701a
uniArchitect <hancho93@gmail.com>
2022-07-09 17:48:48 -0400

    Created new event listener to submit form information into a new object

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ class Book {
             return this.name + ', ' + this.author + ', ' + this.pages + ', Has read? ' + this.hasRead;
         }
     }
-
-    addBookToLibrary() {
-        let newBook = new Book(NAME_INPUT.value, AUTHOR_INPUT.value, PAGE_INPUT.value, READ_INPUT.value);
-        myLibrary = [newBook];
-        console.log(newBook.info());
-        return myLibrary;
-    }
 }
 
 // UI Class - Handles creating new UI for adding books to the library
@@ -43,20 +36,46 @@ class UI {
 
         // For each book called, run the addBookToList method to add it to the page
         // 'book' may need to be 'Book' based on what I've already created
-        books.forEach((book) => UI.addBookToList(book));
+        books.forEach((Book) => UI.addBookToList(Book));
     }
 
-    static addBookToList(book) {
+    static addBookToList(Book) {
+        const list = document.querySelector('.library-background');
 
+        const bookCard = document.createElement('div');
+
+        bookCard.innerHTML = `
+        <p>Title: ${Book.name}</p>
+        <p>Author: ${Book.author}</p>
+        <p>Page Count: ${Book.pages}</p>
+        <p>Has Read? ${Book.hasRead}</p>
+        `;
+
+        list.appendChild(bookCard);
     }
 }
 
 // Store Class - Handles storage (Important for later)
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('.sign-up-field').addEventListener('submit', (e) => {
+    
+    e.preventDefault();
 
+    // Recording form values
+    const name = document.querySelector('#name').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#page-count').value;
+    const hasRead = document.querySelector('#read-status').value;
+
+    const book = new Book(name, author, pages, hasRead);
+
+    console.log(book.info());
+    return myLibrary;
+})
 // Event: Remove a Book
 
 // GLOBAL SCOPE
@@ -70,7 +89,7 @@ const READ_INPUT = document.querySelector('#read-status')
 const SUBMIT_BUTTON = document.querySelector('#book-submit')
 let myLibrary = [];
 
-const book = new Book(NAME_INPUT.value, AUTHOR_INPUT.value, PAGE_INPUT.value, READ_INPUT.value)
+// const book = new Book(NAME_INPUT.value, AUTHOR_INPUT.value, PAGE_INPUT.value, READ_INPUT.value)
 
 // Defined a variable for a created new book
 // const testBook = new Book('NAME_INPUT', 'AUTHOR_INPUT', 'PAGE_INPUT', 'READ_INPUT')
@@ -80,9 +99,3 @@ const book = new Book(NAME_INPUT.value, AUTHOR_INPUT.value, PAGE_INPUT.value, RE
 //     myLibrary = [newBook];
 //     console.log(myLibrary);
 // }
-
-SUBMIT_BUTTON.addEventListener('click', () => {
-    book.addBookToLibrary();
-})
-
-// Create a new class to receive stored books after they have been input by the user

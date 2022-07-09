3dbb780ac396095ee8dcd24d47763bb621c6b635
uniArchitect <hancho93@gmail.com>
2022-07-08 21:44:59 -0400

    Test constructor object to add function for returning book info

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 // GLOBAL SCOPE
 
 // '#' is the id selector
-const NAME_INPUT = document.querySelector('#name') 
+// '.' is the class selector
+const NAME_INPUT = document.querySelector('#name');
 const AUTHOR_INPUT = document.querySelector('#author')
 const PAGE_INPUT = document.querySelector('#page-count')
 const READ_INPUT = document.querySelector('#read-status')
 const SUBMIT_BUTTON = document.querySelector('#book-submit')
 let myLibrary = [];
 
-function book(NAME_INPUT, AUTHOR_INPUT, PAGE_INPUT, READ_INPUT) {
-    this.name = NAME_INPUT;
-    this.author = AUTHOR_INPUT;
-    this.pages = PAGE_INPUT;
-    this.hasRead = READ_INPUT;
+function Book(name, author, pages, hasRead) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+    this.info = function() {
+        return this.name + ', ' + this.author + ', ' + this.pages + ', ' + this.hasRead;
+    }
 }
 
 // Defined a variable for a created new book
 
-let newBook1 = new book('LotR','Tolkien', '1000', 'Yes')
+const newBook = new Book('NAME_INPUT', 'AUTHOR_INPUT', 'PAGE_INPUT', 'READ_INPUT')
 
 // addedBook is what will get pushed into myLibrary
 
-book.prototype.addBookToLibrary = function(addedBook) {
-    myLibrary = [addedBook];
+Book.prototype.addBookToLibrary = function(newBook) {
+    myLibrary = [newBook];
     console.log(myLibrary);
 }
 
 // Create a function that will take the global scope variables in the constructor and define a new variable with all the variables
 // Use the prototype to help??
 
-// SUBMIT_BUTTON.addEventListener('click', () => {
-
-// })
+SUBMIT_BUTTON.addEventListener('click', () => {
+    Book.addBookToLibrary(newBook);
+})
